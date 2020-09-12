515080a832dba66d0d5f8be3ecb9b366d4e12675
Jack Williams <j.williams@ou.edu>
2020-09-12 11:51:55 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        OU CYCLING CLUB
       </header>
+      <body className="App-body">
+        <p>Love the outdoors? Hate running? Try cycling!</p>
+        <p>OU Cycling club is welcoming of all members, whether you're busy training for the Tour de France or still have your training wheels on.</p>
+        <p>
+          We're still working on figuring out group rides, but we plan to have about 3 per week. Rides will differ in length and average speed.
+        </p>
+        <p>
+          Join our <a href="https://groupme.com/join_group/62392643/ByVifG8o">groupme!</a>
+        </p>
+      </body>
     </div>
   );
 }
