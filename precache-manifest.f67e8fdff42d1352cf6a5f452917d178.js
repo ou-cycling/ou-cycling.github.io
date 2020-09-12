576300ab677c53bb0ec79b840a7b9399e7bee90f
Jack Williams <j.williams@ou.edu>
2020-09-12 12:41:29 -0500

    Updates

---
+++ precache-manifest.f67e8fdff42d1352cf6a5f452917d178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36d8b40671492266faf99abfe8aa97c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ed18565b403556a2f57",
+    "url": "/static/css/main.013b65ff.chunk.css"
+  },
+  {
+    "revision": "5d489cb47f10a37afa10",
+    "url": "/static/js/2.5beebad9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5beebad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed18565b403556a2f57",
+    "url": "/static/js/main.e57a4224.chunk.js"
+  },
+  {
+    "revision": "574bc80f0b888d027238",
+    "url": "/static/js/runtime-main.5665c9d1.js"
+  }
+]);
